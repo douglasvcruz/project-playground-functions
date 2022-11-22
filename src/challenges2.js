ba48641403f3557367dd7e9f24b2912a553aa8b4
Douglas <cbdouglasvirissimo@hotmail.com>
2022-11-22 12:23:07 -0300

    11º Requisito

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,27 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function manyFor(numbers) {
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (
+      numbers[i] < 0 || numbers[i] > 9 || numbers.filter((x) => x === numbers[i]).length > 2
+    ) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+}
+
+function generatePhoneNumber(numbers) {
+  if (numbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  const testFor = manyFor(numbers);
+  if (testFor) return testFor;
+
+  let xPhone = '(xx) xxxxx-xxxx';
+  for (let i = 0; i < numbers.length; i += 1) {
+    xPhone = xPhone.replace('x', numbers[i]);
+  }
+  return xPhone;
 }
 
 // Desafio 12
@@ -15,14 +36,17 @@ function triangleCheck(lineA, lineB, lineC) {
 function hydrate(numeros) {
   let quantidade = numeros.match(/\d+/g);
   let soma = 0;
-  for (let i = 0; i < quantidade.length; i += 1) {
-    soma += parseInt(quantidade[i], 10);
-  }
+
+  quantidade.forEach((element) => {
+    soma += parseInt(element, 10);
+  });
   if (soma === 1) {
     return `${soma} copo de água`;
   }
   return `${soma} copos de água`;
 }
+
+console.log(hydrate('4 caipirinhas e 2 cervejas'));
 
 module.exports = {
   generatePhoneNumber,
